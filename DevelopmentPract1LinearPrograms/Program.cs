0d45a35f64b1ac05678eec4fa8aac281004b0802
Захар Голуб <pahargolub2@gmail.com>
2024-01-30 00:00:44 +0300

    Сделаны вторые задания

--- DevelopmentPract1LinearPrograms/Program.cs
+++ DevelopmentPract1LinearPrograms/Program.cs
@@ -24,7 +24,7 @@ namespace DevelopmentPract1LinearPrograms
 
     internal class Program
     {
-        #region Задания
+        #region Задания (первая часть)
         static void Task1_1()
         {
             try
@@ -543,6 +543,70 @@ namespace DevelopmentPract1LinearPrograms
         }
         #endregion
 
+        #region Задания (вторая часть (два задания))
+        static void Task2_5()
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Алгоритм исследования числа");
+                Console.ResetColor();
+
+
+
+                Console.Write("Введите вещественное число A->");
+                double A=Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Целая часть числа {0:f0}:", A);
+                {
+                    string floatingPart = (Math.Floor((A - Math.Floor(A))*Math.Pow(10,4))/Math.Pow(10,4)).ToString();
+                    if (floatingPart.Length > 2)
+                        floatingPart = floatingPart.Substring(2);
+                    else
+                        floatingPart = "0";
+                    Console.WriteLine("Дробная часть числа :"+floatingPart);
+                }
+                Console.WriteLine("Символ, код котрого равен целой части числа A: \"" + (char)(Math.Floor(A))+"\"");
+                Console.WriteLine("Квадратный корень числа A = {0:f4}",Math.Sqrt(A));
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.ToString());
+                Console.ResetColor();
+            }
+        }
+        static void Task2_33()
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Алгоритм вычисления количества краски");
+                Console.ResetColor();
+
+
+                    
+                Console.Write("Введите расход краски (грамм) на 1 квадратный метр->");
+                double расход = Convert.ToDouble(Console.ReadLine());
+                if (расход <= 0)
+                    throw new Exception("Ошибка! Неверный расход краски");
+                Console.Write("Введите площадь стола (квадратные метры)->");
+                double площадьСтола = Convert.ToDouble(Console.ReadLine());
+                if (площадьСтола <= 0)
+                    throw new Exception("Ошибка! Неверная площадь стола!");
+
+                double количествоКраски = площадьСтола * расход;
+                Console.WriteLine("Количество краски (грамм), которое нужно потратить на покраску стола:"+количествоКраски);
+
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.ToString());
+                Console.ResetColor();
+            }
+        }
+        #endregion
+
         static void TaskDescription(ushort number)
         {
             Console.ForegroundColor = ConsoleColor.Green;
@@ -556,6 +620,7 @@ namespace DevelopmentPract1LinearPrograms
         }
         static void Main(string[] args)
         {
+            /*
             TaskDescription(1);
             Task1_1();
             Pause();
@@ -627,10 +692,13 @@ namespace DevelopmentPract1LinearPrograms
             Pause();
             TaskDescription(24);
             Task1_24();
-            Pause();
+            Pause();*/
+
+            Task2_5();
+            Task2_33();
 
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Все задания выполнены!");
+            Console.WriteLine("Все задания первой части выполнены!");
             Console.ReadKey();
             Console.ResetColor();
         }
